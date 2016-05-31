46d0c64642d3fcac1ff2d3ed25d425acff2a2186
Chie <akiba.chie@gmail.com>
2016-05-31 14:42:33 -0700

    Added search function so that when a person chooses what they want to look up, the logos swap to the colored version. Need to figure out how to undo the previous search

--- js/app.js
+++ js/app.js
@@ -1,16 +1,56 @@
 // global variable
 var scholarshipOption;
 var educationOption;
+var logos;
 
-$(document).ready(function(){
-	$('.scholarship li').click(function(){
-		scholarshipOption = $(this).children().html();
-		$('.program').text(scholarshipOption);
-		console.log(scholarshipOption);
-	});
-	$('.education li').click(function(){
-		educationOption = $(this).children().html();
-		$('.education').text(educationOption);
-		console.log(educationOption);
-	});
+$(document).ready(function() {
+    $('.scholarship li').click(function() {
+        scholarshipOption = $(this).children().html();
+        $('.program').text(scholarshipOption);
+    })
+    $('.education li').click(function() {
+        educationOption = $(this).children().html();
+        $('.education-level').text(educationOption);
+    })
+    $('.search').click(function() {
+        if ((scholarshipOption.match('SEARCH ALL')) && (educationOption.match('SEARCH ALL'))) {
+            $('.first').replaceWith('<img src="logos/aci - color.png" alt=aci-color>');
+            $('.sixth').replaceWith('<img src="logos/aci - color.png" alt=aci-color>');
+            $('.eighth').replaceWith('<img src="logos/aci - color.png" alt=aci-color>');
+            $('.second').replaceWith('<img src="logos/acm - color.png" alt=acm-color>');
+            $('.fourth').replaceWith('<img src="logos/acm - color.png" alt=acm-color>');
+            $('.ninth').replaceWith('<img src="logos/acm - color.png" alt=acm-color>');
+            $('.third').replaceWith('<img src="logos/afe color.png" alt=afe-color>');
+            $('.fifth').replaceWith('<img src="logos/afe color.png" alt=afe-color>');
+            $('.seventh').replaceWith('<img src="logos/afe color.png" alt=afe-color>');
+        }
+    })
+    $('.search').click(function() {
+        if ((scholarshipOption.match('SCHOLARSHIP PROGRAM')) && (educationOption.match('HIGH SCHOOL'))) {
+            $('.first').replaceWith('<img src="logos/aci - color.png" alt=aci-color>');
+            $('.sixth').replaceWith('<img src="logos/aci - color.png" alt=aci-color>');
+            $('.eighth').replaceWith('<img src="logos/aci - color.png" alt=aci-color>');
+        }
+    })
+    $('.search').click(function() {
+        if ((scholarshipOption.match('LOAN PROGRAM')) && (educationOption.match('HIGH SCHOOL'))) {
+            $('.second').replaceWith('<img src="logos/acm - color.png" alt=acm-color>');
+            $('.fourth').replaceWith('<img src="logos/acm - color.png" alt=acm-color>');
+            $('.ninth').replaceWith('<img src="logos/acm - color.png" alt=acm-color>');
+        }
+    })
+    $('.search').click(function() {
+        if ((scholarshipOption.match('SCHOLARSHIP PROGRAM')) && (educationOption.match('KINDERGARTEN'))) {
+            $('.third').replaceWith('<img src="logos/afe color.png" alt=afe-color>');
+            $('.fifth').replaceWith('<img src="logos/afe color.png" alt=afe-color>');
+            $('.seventh').replaceWith('<img src="logos/afe color.png" alt=afe-color>')
+        }
+    })
+    $('.search').click(function() {
+        if ((scholarshipOption.match('LOAN PROGRAM')) && (educationOption.match('KINDERGARTEN'))) {
+            $('.sixth').replaceWith('<img src="logos/aci - color.png" alt=aci-color>');
+            $('.second').replaceWith('<img src="logos/acm - color.png" alt=acm-color>');
+            $('.seventh').replaceWith('<img src="logos/afe color.png" alt=afe-color>');
+        }
+    })
 })
