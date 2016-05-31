c927db01541c69ed12e6f863bbbfa9601a82b3df
Chie <akiba.chie@gmail.com>
2016-05-31 01:04:05 -0700

    Added dropdown click function. Need to fix bug though

--- js/app.js
+++ js/app.js
@@ -1,3 +1,14 @@
 $(document).ready(function(){
-	
+	$('.dropdown-content li').click(function(){
+		var scholarshipOption = $(this).children().html();
+		$('.program').text(scholarshipOption);
+		console.log(scholarshipOption);
+		$('.program').off();
+	});
+	$('.dropdown-content li').click(function(){
+		var educationOption = $(this).children().html();
+		$('.education').text(educationOption);
+		console.log(educationOption);
+		$('.education').off();
+	});
 })
